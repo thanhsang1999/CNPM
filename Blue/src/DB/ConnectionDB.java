58218fc0d07851e360da5ae1eb3cec8a267bb185
= <=>
2020-08-10 15:24:58 +0700

    Truong commit

--- Blue/src/DB/ConnectionDB.java
+++ Blue/src/DB/ConnectionDB.java
@@ -15,20 +15,19 @@ public class ConnectionDB {
         if (con==null||con.isClosed()) {
             Class.forName("com.mysql.jdbc.Driver");
 //            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/blue?useUnicode=true&characterEncoding=utf-8", "root", "");
-            con = DriverManager.getConnection("jdbc:mysql://node231729-cnpm2020.j.layershift.co.uk/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "BFAcbz45806");
-//            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "");
+//            con = DriverManager.getConnection("jdbc:mysql://node231729-cnpm2020.j.layershift.co.uk/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "BFAcbz45806");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "");
             return con.createStatement();
         } else {
             return con.createStatement();
         }
-
     }
     public static PreparedStatement prepareStatement(String sql) throws ClassNotFoundException, SQLException  {
     	if (con==null||con.isClosed()) {
     		Class.forName("com.mysql.jdbc.Driver");
 //    		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project?useUnicode=true&characterEncoding=utf-8", "root", "");
-    		con = DriverManager.getConnection("jdbc:mysql://node231729-cnpm2020.j.layershift.co.uk/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "BFAcbz45806");
-//    		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "");
+//    		con = DriverManager.getConnection("jdbc:mysql://node231729-cnpm2020.j.layershift.co.uk/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "BFAcbz45806");
+    		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webbandoan?useUnicode=true&characterEncoding=utf-8", "root", "");
     		return con.prepareStatement(sql);
     	} else {
     		return con.prepareStatement(sql);
